5f192610f1a3b7b8341c92156a06c2f30f665612
Jason McTigue <jasonmctigue@live.ie>
2017-04-22 17:46:17 +0100

    added valid rpn

--- countdown.rkt
+++ countdown.rkt
@@ -13,29 +13,42 @@
 (define b 25)
 (define t 125)
 
-(+ a b)
-(+ b a)
-(- a b)
-(- b a)
-(* a b)
-(* b a)
-(/ a b)
-(/ b a)
+;(+ a b)
+;(+ b a)
+;(- a b)
+;(- b a)
+;(* a b)
+;(* b a)
+;(/ a b)
+;(/ b a)
 
 
 ;Reverse polish notation from class
 (define start (list  -1 -1 -1 -1 1 1 1 1 1))
 
+;This function removes duplicates from the list
 (define per8(remove-duplicates(permutations start)))
 
-(define (to-rpn l) append (list 1 1) l (list -1))
-(car per8)
+;two 1s are added to the front of the list and then a -1 is added to the  end of the list
+(define (to-rpn l) (append (list 1 1) l (list -1)))
+;(car per8)
+
 (to-rpn (car per8))
 
+;Takes all arangements with 2 numbers at the start
 (map to-rpn per8)
 
 
-
+(define (valid-rpn? e (s 0)) ;default value is 0
+  (if (null? e)
+      (if (= s 1) #t #f);if there is one number on the stack return true
+      ;if not take car of the list and check if it is a number or opp.
+      (if (= (car e) 1)
+          (valid-rpn? (cdr e) (+ s 1));adds one to the stack
+      ;checks to make sure s has at least two numbers else return false
+      (if (< s 2)
+          #f
+          (#t)))))
 
 
 
